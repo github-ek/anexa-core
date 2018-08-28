52e98910743c4c2d69ddb018de58aeef275751d1
albam-osorio <arosorio@gmail.com>
2018-08-28 05:46:12 -0500

    1

--- anexa-core-services/src/main/java/com/anexa/core/services/crud/api/QueryByCodigoService.java
+++ anexa-core-services/src/main/java/com/anexa/core/services/crud/api/QueryByCodigoService.java
@@ -2,8 +2,11 @@ package com.anexa.core.services.crud.api;
 
 import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.anexa.core.domain.ObjectWithCode;
 
+@Transactional(readOnly = true)
 public interface QueryByCodigoService<M extends ObjectWithCode<ID>, ID> {
 
 	Optional<M> findByCodigo(String codigo);
